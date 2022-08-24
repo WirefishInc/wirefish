bda5cb247f3b3d3e71b9974e1d40849ecd8c2c9e
Stefano De Venuto <stefanodevenuto@hotmail.com>
2022-08-24 10:47:31 +0200

    [RUST] Refactors the Parsing code
    The code is now inside another module, leaving the main just the
    responsibility to send the parsed packets.
    
    The module splits the code according with the current level of the
    packet. For now, just network (src/parser/network.rs) and transport
    (src/parser/transport.rs).

---
+++ src-tauri/src/parser/mod.rs
@@ -0,0 +1,74 @@
+pub mod network;
+pub mod transport;
+
+use pnet::packet::ethernet::{EtherTypes, EthernetPacket};
+use pnet::packet::Packet;
+use pnet::util::MacAddr;
+use serde::Serialize;
+
+use crate::parser::network::*;
+
+// JSON Packet fields:
+// - Type (TCP, UDP, ...)
+// - MAC Source
+// - MAC Destination
+// - IP Source
+// - IP Destination
+// - Length
+// - Additional Info
+// - (?) Payload
+
+#[derive(Serialize)]
+pub struct GenericPacket {
+    packet_type: String,
+    mac_source: MacAddr,
+    mac_destination: MacAddr,
+    ip_source: String,
+    ip_destination: String,
+    length: usize,
+    info: String,
+    payload: Option<Vec<u8>>,
+}
+
+impl GenericPacket {
+    pub fn new(
+        packet_type: String,
+        mac_source: MacAddr,
+        mac_destination: MacAddr,
+        ip_source: String,
+        ip_destination: String,
+        length: usize,
+        info: String,
+        payload: Option<Vec<u8>>,
+    ) -> Self {
+        GenericPacket {
+            packet_type,
+            mac_source,
+            mac_destination,
+            ip_source,
+            ip_destination,
+            length,
+            info,
+            payload,
+        }
+    }
+}
+
+pub fn handle_ethernet_frame(ethernet: &EthernetPacket) -> Option<GenericPacket> {
+    match ethernet.get_ethertype() {
+        EtherTypes::Ipv4 => handle_ipv4_packet(ethernet),
+        EtherTypes::Ipv6 => handle_ipv6_packet(ethernet),
+        EtherTypes::Arp => handle_arp_packet(ethernet),
+        _ => {
+            println!(
+                "[]: Unknown packet: {} > {}; ethertype: {:?} length: {}",
+                ethernet.get_source(),
+                ethernet.get_destination(),
+                ethernet.get_ethertype(),
+                ethernet.packet().len()
+            );
+
+            None
+        }
+    }
+}
