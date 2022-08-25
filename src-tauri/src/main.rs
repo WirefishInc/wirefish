763ee4901e4693cf965b9e74484f1c8a630e636c
Stefano De Venuto <stefanodevenuto@hotmail.com>
2022-08-25 18:15:28 +0200

    [RUST] Refactors Tauri's State and adds basic logging
    The entire state is now wrapped inside an Arc-Mutex combo, as opposed as
    before where only some field where protected.
    
    Adds some logging messages on src/main.rs, using the env_logger crate.
    For now, until we find a way to tell tauri how to launch cargo (with
    which env variables), in order to see the messages we have to create a
    `.env` file an write `RUST_LOG=app` inside.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,12 @@
 extern crate pnet;
 extern crate sniffer_parser;
 
+use log::{error, info};
+use dotenv;
+
 use pnet::datalink::Channel::Ethernet;
 use pnet::datalink::{self, DataLinkReceiver, NetworkInterface};
-use pnet::packet::ethernet::{EthernetPacket};
+use pnet::packet::ethernet::EthernetPacket;
 
 use std::sync::{Arc, Mutex};
 use tauri::{Manager, Window, Wry};
@@ -11,33 +14,36 @@ use tauri_awesome_rpc::{AwesomeEmit, AwesomeRpc};
 
 use sniffer_parser::handle_ethernet_frame;
 
-struct SniffingState {
-    interface_channel: Arc<Mutex<Option<(bool, Box<dyn DataLinkReceiver>)>>>,
+struct SniffingInfoState(Arc<Mutex<SniffingInfo>>);
+struct SniffingInfo {
+    interface_channel: Option<Box<dyn DataLinkReceiver>>,
+    interface_name: Option<String>,
+    is_sniffing: bool,
 }
 
-impl SniffingState {
+impl SniffingInfo {
     fn new() -> Self {
-        SniffingState {
-            interface_channel: Arc::new(Mutex::new(None)),
+        SniffingInfo {
+            interface_channel: None,
+            interface_name: None,
+            is_sniffing: false,
         }
     }
 }
 
 #[tauri::command]
 fn get_interfaces_list() -> Vec<String> {
-    println!("Interface retrieval started");
     let interfaces = datalink::interfaces()
         .into_iter()
         .map(|i| i.description)
         .collect::<Vec<String>>();
-    println!("Interfaces: {:?}", interfaces);
+    info!("Interfaces retrieved: {:#?}", interfaces);
+
     interfaces
 }
 
 #[tauri::command]
-fn select_interface(state: tauri::State<SniffingState>, interface_name: String) {
-    println!("Interface name: {}", interface_name);
-
+fn select_interface(state: tauri::State<SniffingInfoState>, interface_name: String) {
     let interface_names_match = |iface: &NetworkInterface| iface.description == interface_name;
 
     // Find the network interface with the provided name
@@ -47,6 +53,8 @@ fn select_interface(state: tauri::State<SniffingState>, interface_name: String) 
         .filter(interface_names_match)
         .next()
         .unwrap();
+
+    info!("Interface selected: {}", interface_name);
 
     // Create a new channel, dealing with layer 2 packets
     let (_, rx) = match datalink::channel(&interface, Default::default()) {
@@ -58,62 +66,70 @@ fn select_interface(state: tauri::State<SniffingState>, interface_name: String) 
         ),
     };
 
-    let mut interface_channel = state.interface_channel.lock().expect("Poisoned lock");
-    *interface_channel = Some((false, rx));
+    let mut sniffing_state = state.0.lock().expect("Poisoned lock");
+    sniffing_state.interface_channel = Some(rx);
+    sniffing_state.interface_name = Some(interface_name);
 
-    println!("Selected!");
+    info!("[{}] Channel created", sniffing_state.interface_name.as_ref().unwrap());
 }
 
 #[tauri::command]
-fn start_sniffing(state: tauri::State<SniffingState>, window: Window<Wry>) {
-    // Set sniffing to true
-    state
-        .interface_channel
-        .lock()
-        .expect("Poisoned lock")
-        .as_mut()
-        .unwrap()
-        .0 = true;
+fn start_sniffing(state: tauri::State<SniffingInfoState>, window: Window<Wry>) {
+    let mut sniffing_state = state.0.lock().expect("Poisoned lock");
 
-    let channel = Arc::clone(&state.interface_channel);
+    if sniffing_state.interface_name.is_none() {
+        error!("Start sniffing without prior selection of the inteface");
+        return;
+    }
+
+    sniffing_state.is_sniffing = true;
+    info!(
+        "[{}] Sniffing started",
+        sniffing_state.interface_name.as_ref().unwrap()
+    );
+
+    let ss = Arc::clone(&state.0);
     std::thread::spawn(move || {
         loop {
-            let mut channel = channel.lock().expect("Poisoned lock");
-            if channel.as_mut().is_none() || !channel.as_mut().unwrap().0 {
+            let mut sniffing_state = ss.lock().expect("Poisoned lock");
+
+            if !sniffing_state.is_sniffing {
                 break;
             }
 
-            match channel.as_mut().unwrap().1.next() {
+            match sniffing_state.interface_channel.as_mut().unwrap().next() {
                 Ok(packet) => {
                     let new_packet = handle_ethernet_frame(&EthernetPacket::new(packet).unwrap());
 
                     if let Some(new_packet) = new_packet {
-                        window.state::<AwesomeEmit>().emit(
-                            "main",
-                            "packet_received",
-                            new_packet,
-                        );
+                        window
+                            .state::<AwesomeEmit>()
+                            .emit("main", "packet_received", new_packet);
                     }
-                },
+                }
                 Err(e) => {
                     // If an error occurs, we can handle it here
+                    error!("An error occurred while reading");
                     panic!("An error occurred while reading: {}", e);
                 }
             }
 
-            drop(channel);
+            drop(sniffing_state);
         }
     });
 }
 
 #[tauri::command]
-fn stop_sniffing(state: tauri::State<SniffingState>) {
-    let mut channel = state.interface_channel.lock().expect("Poisoned lock");
-    channel.as_mut().unwrap().0 = false;
-    println!("Stop Sniffing :(");
+fn stop_sniffing(state: tauri::State<SniffingInfoState>) {
+    let mut sniffing_state = state.0.lock().expect("Poisoned lock");
+    sniffing_state.is_sniffing = false;
+    info!("[{}] Sniffing stopped", sniffing_state.interface_name.as_ref().unwrap());
 }
 
 fn main() {
+    dotenv::dotenv().ok();
+    env_logger::init();
+
     let awesome_rpc = AwesomeRpc::new(vec!["tauri://localhost", "http://localhost:*"]);
 
     tauri::Builder::default()
@@ -122,7 +138,7 @@ fn main() {
             awesome_rpc.start(app.handle());
             Ok(())
         })
-        .manage(SniffingState::new())
+        .manage(SniffingInfoState(Arc::new(Mutex::new(SniffingInfo::new()))))
         .invoke_handler(tauri::generate_handler![
             start_sniffing,
             stop_sniffing,
