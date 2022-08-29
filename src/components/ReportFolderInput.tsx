3edfd2583efeccc29b734d57198c1d74980c1357
Alberto Baroso <albertobaroso999@gmail.com>
2022-08-29 20:39:03 +0200

    Fixed app crashing if no folder is selected

--- src/components/ReportFolderInput.tsx
+++ src/components/ReportFolderInput.tsx
@@ -2,15 +2,17 @@ import {FormControl, FormHelperText, TextField} from "@mui/material";
 import {SniffingStatus} from "../types/sniffing";
 import {open} from '@tauri-apps/api/dialog';
 import {appDir} from '@tauri-apps/api/path';
+import {useState} from "react";
 
 const selectDirectory = async (originalDirectory: string | string[] | null) => {
     let directory = originalDirectory;
     try {
-        directory = await open({
+        let result = await open({
             directory: true,
-            multiple: true,
+            multiple: false,
             defaultPath: await appDir(),
         });
+        directory = result || directory;
     } catch (NoDirectorySelected) {}
     return directory;
 }
@@ -26,15 +28,23 @@ const ReportFolderInput = ({
                                setReportFolder,
                                sniffingStatus
                            }: ReportFolderInputProps) => {
+
+    let [selectingFolder, setSelectingFolder] = useState(false);
+    const setReportFolderWrapper = async () => {
+        setSelectingFolder(true);
+        setReportFolder(await selectDirectory(reportFolder));
+        setSelectingFolder(false);
+    }
+
     return (
         <FormControl error={reportFolder.length < 1} variant="standard" sx={{mt: 3, width: "100%"}}>
             <TextField
                 label="Report folder"
                 value={reportFolder}
                 error={reportFolder.length < 1}
-                disabled={sniffingStatus !== SniffingStatus.Inactive}
-                onChange={async (e) => setReportFolder(await selectDirectory(reportFolder))}
-                onClick={async (e) => setReportFolder(await selectDirectory(reportFolder))}
+                disabled={sniffingStatus !== SniffingStatus.Inactive || selectingFolder}
+                onChange={setReportFolderWrapper}
+                onClick={setReportFolderWrapper}
             />
             {
                 reportFolder.length < 1 &&
