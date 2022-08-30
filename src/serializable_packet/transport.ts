1bab6c6a2575e544279b74646f5e9ef0f20beeea
CHRISTIAN DAMIANO CAGNAZZO  s304045 <s304045@studenti.polito.it>
2022-08-30 12:33:31 +0200

    [TS] Added transport layer packet (tcp)

---
+++ src/serializable_packet/transport.ts
@@ -0,0 +1,44 @@
+import {SerializablePacket} from "../types/sniffing";
+
+export class TcpPacket implements SerializablePacket {
+    source: number;
+    destination: number;
+    sequence: number;
+    acknowledgement: number;
+    data_offset: number;
+    reserved: number;
+    flags: number;
+    window: number;
+    checksum: number;
+    urgent_ptr: number;
+    options: [];
+    payload: [];
+
+    constructor(
+        source: number,
+        destination: number,
+        sequence: number,
+        acknowledgement: number,
+        data_offset: number,
+        reserved: number,
+        flags: number,
+        window: number,
+        checksum: number,
+        urgent_ptr: number,
+        options: [],
+        payload: [],
+    ) {
+        this.source = source;
+        this.destination = destination;
+        this.sequence = sequence;
+        this.acknowledgement = acknowledgement;
+        this.data_offset = data_offset;
+        this.reserved = reserved;
+        this.flags = flags;
+        this.window = window;
+        this.checksum = checksum;
+        this.urgent_ptr = urgent_ptr;
+        this.options = options;
+        this.payload = payload;
+    }
+}
