f86ccf562e722dc0e92a4fbad0aa2f249e9a23ff
Stefano De Venuto <stefanodevenuto@hotmail.com>
2022-08-31 10:02:48 +0200

    [RUST] Adds text and image based HTTP Payload parsing

--- src-tauri/sniffer_parser/src/application.rs
+++ src-tauri/sniffer_parser/src/application.rs
@@ -1,12 +1,17 @@
+use std::str::from_utf8;
 use std::{cell::RefCell, collections::HashMap, net::IpAddr};
 
+use mime::Mime;
+
 use crate::serializable_packet::application::{
-    SerializableHttpRequestPacket, SerializableHttpResponsePacket,
+    HttpContentType, SerializableHttpRequestPacket, SerializableHttpResponsePacket,
 };
 use crate::serializable_packet::ParsedPacket;
 use crate::SerializablePacket;
 
-pub enum HttpType {
+const CONTENT_TYPE: &str = "Content-Type";
+
+pub enum HttpPacketType {
     Request,
     Response,
 }
@@ -23,7 +28,7 @@ pub fn handle_http_packet(
     source_port: u16,
     dest_ip: IpAddr,
     dest_port: u16,
-    http_type: HttpType,
+    http_type: HttpPacketType,
     packet: &[u8],
     parsed_packet: &mut ParsedPacket,
 ) {
@@ -37,20 +42,33 @@ pub fn handle_http_packet(
         let mut headers = [httparse::EMPTY_HEADER; 64];
 
         match http_type {
-            HttpType::Request => {
+            HttpPacketType::Request => {
                 let mut request = httparse::Request::new(&mut headers);
                 let status = request.parse(current_payload);
 
                 if let Ok(status) = status {
                     if status.is_complete() {
+                        let mut parsed_payload = HttpContentType::None;
+
+                        if !current_payload[status.unwrap()..].is_empty() {
+                            parsed_payload = parse_http_payload(
+                                &current_payload[status.unwrap()..],
+                                request
+                                    .headers
+                                    .iter()
+                                    .find(|h| h.name == CONTENT_TYPE)
+                                    .and_then(|h| from_utf8(h.value).unwrap().parse::<Mime>().ok()),
+                            );
+                        }
+
                         println!(
-                            "[]: HTTP Request Packet: {:?} {:?} {:?}; Headers: {:?}",
-                            request.method, request.path, request.version, request.headers,
+                            "[]: HTTP Request Packet: {:?} {:?} {:?}; Headers: {:?}; Payload: {:?}",
+                            request.method, request.path, request.version, request.headers, parsed_payload
                         );
 
                         parsed_packet.set_application_layer_packet(Some(
                             SerializablePacket::HttpRequestPacket(
-                                SerializableHttpRequestPacket::from(&request),
+                                SerializableHttpRequestPacket::new(&request, parsed_payload),
                             ),
                         ));
 
@@ -58,20 +76,33 @@ pub fn handle_http_packet(
                     }
                 }
             }
-            HttpType::Response => {
+            HttpPacketType::Response => {
                 let mut response = httparse::Response::new(&mut headers);
                 let status = response.parse(current_payload);
 
                 if let Ok(status) = status {
                     if status.is_complete() {
+                        let mut parsed_payload = HttpContentType::None;
+
+                        if !current_payload[status.unwrap()..].is_empty() {
+                            parsed_payload = parse_http_payload(
+                                &current_payload[status.unwrap()..],
+                                response
+                                    .headers
+                                    .iter()
+                                    .find(|h| h.name == CONTENT_TYPE)
+                                    .and_then(|h| from_utf8(h.value).unwrap().parse::<Mime>().ok()),
+                            );
+                        }
+
                         println!(
-                            "[]: HTTP Response Packet: {:?} {:?} {:?}; Headers: {:?}",
-                            response.version, response.code, response.reason, response.headers
+                            "[]: HTTP Response Packet: {:?} {:?} {:?}; Headers: {:?}; Payload: {:?}",
+                            response.version, response.code, response.reason, response.headers, parsed_payload
                         );
 
                         parsed_packet.set_application_layer_packet(Some(
                             SerializablePacket::HttpResponsePacket(
-                                SerializableHttpResponsePacket::from(&response),
+                                SerializableHttpResponsePacket::new(&response, parsed_payload),
                             ),
                         ));
 
@@ -81,4 +112,20 @@ pub fn handle_http_packet(
             }
         }
     });
+}
+
+fn parse_http_payload(payload: &[u8], mime: Option<Mime>) -> HttpContentType {
+    if mime.is_none() {
+        return HttpContentType::Unknown(payload.to_vec());
+    }
+
+    let mime = mime.unwrap();
+    return match (mime.type_(), mime.subtype()) {
+        (mime::TEXT, _) => match from_utf8(payload) {
+            Ok(string) => HttpContentType::Text(string.to_owned()),
+            _ => HttpContentType::Unknown(payload.to_vec()),
+        },
+        (mime::IMAGE, _) => HttpContentType::Image(payload.to_vec()),
+        _ => HttpContentType::Unknown(payload.to_vec()),
+    };
 }
